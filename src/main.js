56700774d4c4cce7f72843423f7c3a1f12fbee80
Nathan Bakirci <nathan@virtru.com>
2017-08-19 15:24:15 -0400

    Added package.json, eslint

---
+++ src/main.js
@@ -0,0 +1,79 @@
+let volCapSortOrder = true;
+
+// Simple $ selector, who needs jQuery anyway?
+function $(q) {
+  const elems = document.querySelectorAll(q);
+  return elems.length === 1 ? elems[0] : elems;
+}
+
+function sortTable(table, col, reverse) {
+  const tb = table.tBodies[0];
+  const reverser = -((+reverse) || -1);
+  let tr = Array.prototype.slice.call(tb.rows, 0);
+  let i;
+
+  tr = tr.sort((a, b) => reverser
+  * (parseFloat(a.cells[col].textContent.trim())
+  - (parseFloat(b.cells[col].textContent.trim()))));
+
+  for (i = 0; i < tr.length; i += 1) {
+    tb.appendChild(tr[i]);
+  }
+
+  volCapSortOrder = !volCapSortOrder;
+}
+
+/**
+ * Adds the new header to the table.
+ * @param {*}  
+ * @param {*}  
+ */
+function addNewHeader($table, $currencyHeader) {
+  const $lastHeader = $currencyHeader.lastChild;
+  const $newColumnHeader = $lastHeader.cloneNode(true);
+  $newColumnHeader.className = 'sortable text-right sorting';
+  $newColumnHeader.attributes['aria-label'].value = 'Vol / Cap';
+  $newColumnHeader.id = 'th-vol-cap';
+  $newColumnHeader.innerHTML = 'Volume / Cap';
+  $newColumnHeader.addEventListener('click', () => {
+    sortTable($table, 7, volCapSortOrder);
+  });
+  $currencyHeader.insertBefore($newColumnHeader, $lastHeader);
+}
+
+function addNewColumn($row) {
+  const VOL_CAP_MAX = 20;
+  const $neighbor = $row.children[$row.childElementCount - 1];
+  const cap = parseFloat($row.querySelector('.market-cap').attributes['data-usd'].value);
+  const vol = parseFloat($row.querySelector('a[class="volume"]').attributes['data-usd'].value);
+  const volToCap = (((vol / cap) * 100).toFixed(2));
+  const colorRatio = (Math.min(volToCap, VOL_CAP_MAX) / VOL_CAP_MAX) * 45;
+  const colorVal = `hsl(140, 100%, ${colorRatio}%)`;
+
+  const $newColumnCell = document.createElement('td');
+  $newColumnCell.className = 'no-wrap text-right';
+  $newColumnCell.innerHTML = `${volToCap}%`;
+  $newColumnCell.style.color = colorVal;
+  $newColumnCell.style.width = '112px';
+  $row.insertBefore($newColumnCell, $neighbor);
+}
+
+const $tableContainer = $('.container .row .col-lg-10');
+const $table = $tableContainer.querySelector('table');
+const $ads = $('#icobanner-wrapper, #leaderboard, .container .col-lg-2');
+const $currencyHeader = $('#currencies_wrapper table thead tr');
+const $currencyRows = $('#currencies_wrapper table tbody tr');
+
+// Hide ads!
+for (let i = 0; i < $ads.length; i += 1) {
+  $ads[i].style.display = 'none';
+}
+
+// Expand the main table to fill the entire window
+$tableContainer.style.width = '100%';
+
+// Enhance currency data
+addNewHeader($table, $currencyHeader);
+for (let i = 0; i < $currencyRows.length; i += 1) {
+  addNewColumn($currencyRows[i]);
+}
