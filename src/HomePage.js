194e9b2e6b866bca0d0e49c5febbbb7de3ceee89
Nathan Bakirci <nathan@virtru.com>
2017-09-15 15:43:57 -0400

    Added vol-cap on all page

--- src/HomePage.js
+++ src/HomePage.js
@@ -20,9 +20,15 @@ function sortTable(table, col, reverse) {
   let tr = Array.prototype.slice.call(tb.rows, 0);
   let i;
 
-  tr = tr.sort((a, b) => reverser
-  * (parseFloat(a.cells[col].textContent.trim())
-  - (parseFloat(b.cells[col].textContent.trim()))));
+  tr = tr.sort((a, b) => {
+    let aVal = parseFloat(a.cells[col].textContent.trim());
+    let bVal = parseFloat(b.cells[col].textContent.trim());
+
+    aVal = isNaN(aVal) ? 0 : aVal;
+    bVal = isNaN(bVal) ? 0 : bVal;
+
+    return reverser * (aVal - bVal);
+  });
 
   for (i = 0; i < tr.length; i += 1) {
     tb.appendChild(tr[i]);
@@ -36,25 +42,26 @@ function sortTable(table, col, reverse) {
  * @param {*}  
  * @param {*}  
  */
-function addNewHeader($table, $currencyHeader) {
-  const $lastHeader = $currencyHeader.lastChild;
-  const $newColumnHeader = $lastHeader.cloneNode(true);
+function addNewHeader($table, $currencyHeader, columnOffset) {
+  const $neighbor = $currencyHeader.children[$currencyHeader.childElementCount - columnOffset];
+  const $newColumnHeader = $neighbor.cloneNode(true);
   $newColumnHeader.className = 'sortable text-right sorting';
   $newColumnHeader.attributes['aria-label'].value = 'Vol / Cap';
   $newColumnHeader.id = 'th-vol-cap';
   $newColumnHeader.innerHTML = 'Volume / Cap';
   $newColumnHeader.addEventListener('click', () => {
-    sortTable($table, 7, volCapSortOrder);
+    sortTable($table, $currencyHeader.childElementCount - columnOffset - 1, volCapSortOrder);
   });
-  $currencyHeader.insertBefore($newColumnHeader, $lastHeader);
+  $currencyHeader.insertBefore($newColumnHeader, $neighbor);
 }
 
-function addNewColumn($row) {
+function addNewColumn($row, columnOffset) {
   const VOL_CAP_MAX = 20;
-  const $neighbor = $row.children[$row.childElementCount - 1];
+  const $neighbor = $row.children[$row.childElementCount - columnOffset];
   const cap = parseFloat($row.querySelector('.market-cap').attributes['data-usd'].value);
   const vol = parseFloat($row.querySelector('a[class="volume"]').attributes['data-usd'].value);
   const volToCap = (((vol / cap) * 100).toFixed(2));
+  const volToCapStr = isNaN(volToCap) ? '?' : `${volToCap}%`;
   let colorRatio;
   let alpha;
 
@@ -70,16 +77,16 @@ function addNewColumn($row) {
 
   const $newColumnCell = document.createElement('td');
   $newColumnCell.className = 'no-wrap text-right';
-  $newColumnCell.innerHTML = `${volToCap}%`;
+  $newColumnCell.innerHTML = volToCapStr;
   $newColumnCell.style.color = colorVal;
   $newColumnCell.style.width = '112px';
   $row.insertBefore($newColumnCell, $neighbor);
 }
 
-function updateMainTable($table, $currencyHeader, $currencyRows) {
-  addNewHeader($table, $currencyHeader);
+function updateMainTable($table, $currencyHeader, $currencyRows, columnOffset) {
+  addNewHeader($table, $currencyHeader, columnOffset);
   for (let i = 0; i < $currencyRows.length; i += 1) {
-    addNewColumn($currencyRows[i]);
+    addNewColumn($currencyRows[i], columnOffset);
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ const $tableContainer = $('.container .row .col-lg-10');
 const $table = $tableContainer.querySelector('table');
 const $currencyHeader = $('#currencies_wrapper table thead tr');
 const $currencyRows = $('#currencies_wrapper table tbody tr');
+const $currencyAllHeader = $('#currencies-all_wrapper table thead tr');
+const $currencyAllRows = $('#currencies-all_wrapper table tbody tr');
 const $coinPageTitle = $('.col-xs-6.col-sm-4.col-md-4');
 const $coinPageLinkList = $('.col-xs-4.col-sm-4 .list-unstyled');
 
@@ -14,7 +16,12 @@ $tableContainer.style.width = '100%';
 
 // Enhance main page currency data
 if ($table && $currencyHeader) {
-  HomePage.updateMainTable($table, $currencyHeader, $currencyRows);
+  HomePage.updateMainTable($table, $currencyHeader, $currencyRows, 1);
+}
+
+// Enhance all page currency data
+if ($table && $currencyAllHeader) {
+  HomePage.updateMainTable($table, $currencyAllHeader, $currencyAllRows, 3);
 }
 
 // Add the TradingView link to coin pages
